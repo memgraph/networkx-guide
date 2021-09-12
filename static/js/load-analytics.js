22d7054c4d63c6a6f3318700d7982c284044e64a
Ivan Despot <ivan.g.despot@outlook.com>
2021-09-12 18:51:20 +0200

    Update Docusaurus version

--- static/js/load-analytics.js
+++ static/js/load-analytics.js
@@ -1,10 +1,84 @@
-!function () {
-  var analytics = window.analytics = window.analytics || []; if (!analytics.initialize) if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice."); else {
-    analytics.invoked = !0; analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "reset", "group", "track", "ready", "alias", "debug", "page", "once", "off", "on", "addSourceMiddleware", "addIntegrationMiddleware", "setAnonymousId", "addDestinationMiddleware"]; analytics.factory = function (e) { return function () { var t = Array.prototype.slice.call(arguments); t.unshift(e); analytics.push(t); return analytics } }; for (var e = 0; e < analytics.methods.length; e++) { var key = analytics.methods[e]; analytics[key] = analytics.factory(key) } analytics.load = function (key, e) { var t = document.createElement("script"); t.type = "text/javascript"; t.async = !0; t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js"; var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(t, n); analytics._loadOptions = e }; analytics._writeKey = "rYGVGUlp4Tx6Brh9OikQDcGwmxRXtHnO"; analytics.SNIPPET_VERSION = "4.13.2";
-    analytics.load("rYGVGUlp4Tx6Brh9OikQDcGwmxRXtHnO");
-    analytics.page();
-  }
-}();
+!(function () {
+  var analytics = (window.analytics = window.analytics || []);
+  if (!analytics.initialize)
+    if (analytics.invoked)
+      window.console &&
+        console.error &&
+        console.error("Segment snippet included twice.");
+    else {
+      analytics.invoked = !0;
+      analytics.methods = [
+        "trackSubmit",
+        "trackClick",
+        "trackLink",
+        "trackForm",
+        "pageview",
+        "identify",
+        "reset",
+        "group",
+        "track",
+        "ready",
+        "alias",
+        "debug",
+        "page",
+        "once",
+        "off",
+        "on",
+        "addSourceMiddleware",
+        "addIntegrationMiddleware",
+        "setAnonymousId",
+        "addDestinationMiddleware",
+      ];
+      analytics.factory = function (e) {
+        return function () {
+          var t = Array.prototype.slice.call(arguments);
+          t.unshift(e);
+          analytics.push(t);
+          return analytics;
+        };
+      };
+      for (var e = 0; e < analytics.methods.length; e++) {
+        var key = analytics.methods[e];
+        analytics[key] = analytics.factory(key);
+      }
+      analytics.load = function (key, e) {
+        var t = document.createElement("script");
+        t.type = "text/javascript";
+        t.async = !0;
+        t.src =
+          "https://cdn.segment.com/analytics.js/v1/" +
+          key +
+          "/analytics.min.js";
+        var n = document.getElementsByTagName("script")[0];
+        n.parentNode.insertBefore(t, n);
+        analytics._loadOptions = e;
+      };
+      analytics._writeKey = "rYGVGUlp4Tx6Brh9OikQDcGwmxRXtHnO";
+      analytics.SNIPPET_VERSION = "4.13.2";
+      analytics.load("rYGVGUlp4Tx6Brh9OikQDcGwmxRXtHnO");
+      analytics.page();
+    }
+})();
+
+!function (h, o, t, j, a, r) {
+  h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+  h._hjSettings = { hjid: 1822641, hjsv: 6 };
+  a = o.getElementsByTagName('head')[0];
+  r = o.createElement('script'); r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+}(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+
+!function (f, b, e, v, n, t, s) {
+  if (f.fbq) return; n = f.fbq = function () {
+    n.callMethod ?
+      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+  };
+  if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+  n.queue = []; t = b.createElement(e); t.async = !0;
+  t.src = v; s = b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t, s)
+}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
 !function (ss) {
   window.ldfdr = function () { (ldfdr._q = ldfdr._q || []).push([].slice.call(arguments).concat(ss)); };
